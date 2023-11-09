8339d671596dead9ff3341cfbe50d1f763fa9816
hbdmom <150239343+hbdmom@users.noreply.github.com>
2023-11-09 15:40:24 +0800

    Update message.js

--- message.js
+++ message.js
@@ -4,15 +4,15 @@ function generateWord()
       ["Dear Mum,","Happy Birthday. Thank you for always loving Kael and I. I appreciate all the big and small things you do for us, and am grateful for you. I know I don’t say it, but I do L-word you. Hope you have a good day", "Kindly,","Carys"],
       ["Dear Kennedee,","It’s Scorpio season! Another year around the sun, happy birthday!", "Sincerely Yours,","Courtney & James"],
       ["Dear Aunty Bubba,","Happy Birthday!! Have the best day. Lots of love.","Sincerely Yours,","Carter-James"],
-      ["Dear Ken,","Haaaaappiest (I won’t mention the number) Birthday!!!😘🥳 I hope that you have a great day today! Thank you for being you, and always reminding me how much you love and care about me - I’ll never have to question it! I hope that you know that I love you heaps & care about you too!❤️ I hope this trip around the sun brings you more happiness, abundance & growth","Love Always,","Lebron & Leyton\n ❤️❤️❤️❤️❤️"],
-      ["Dear Kennedee,","Happy birthday, have a great day. 40 next year 🎉 .\n Kael said he’ll cook you dinner.","Sincerely Yours,","Rob, mum n dad"],
+      ["Dear Ken,","Haaaaappiest (I won’t mention the number) Birthday!!!😘🥳 I hope that you have a great day today! Thank you for being you, and always reminding me how much you love and care about me - I’ll never have to question it! I hope that you know that I love you heaps & care about you too!❤️ I hope this trip around the sun brings you more happiness, abundance & growth","Love Always,","Lebron & Leyton ♥️♥️♥️♥️♥️♥"],
+      ["Dear Kennedee,","Happy birthday, have a great day. 40 next year 🎉 .Kael said he’ll cook you dinner.","Sincerely Yours,","Rob, mum n dad"],
       ["Dear Aunty Ken,","Happy birthday 🥳 hope you have a wonderful day, filled with people, moments and edible things that bring you joy 😅 Sorry we can't be there (as usual 👀), but looking forward to having you here in NZ soon enough. Take time to smell the flowers today and be kind to yourself 😊 ","Love you heaps and always,","Connor, Hayley and Little Aila"],
       ["Dear Kael's Mum,","Happy birthday lil Ken. I remembered your bday this year your welcome 🤣 hope you have a wonderful day love you.","Sincerely Yours,","Shonn"],
-      ["Dear Mum,","Happy Birthday! Thank you for the best mom ever and my only mom. I appreciate everything you do for me and Carys. I love you alot. I hope you like my E-Card I made for you with the help of everyone who loves you.","Your Favourite Son,","Kael ♥️♥️♥️♥️"],
-      ["To my Darling Girl Happy Birthday 🎂 sweetheart!!","Wishing you an awesome day & an exciting year ahead.\nThank you for all you do for me, Dad & our family..\nI love ❤️ you to the moon 🌙 & back.. & more 🌌 ","Love you heaps!","Your Mum.. \n🥰❤️🥰❤️🥰❤️"],
+      ["Dear Mum,","Happy Birthday! Thank you for the best mom ever and my only mom. I appreciate everything you do for me and Carys. I love you alot. I hope you like my E-Card I made for you with the help of everyone who loves you.","Your Favourite Son,","Kael ♥️"],
+      ["To my darling girl,","Happy Birthday 🎂 sweetheart!! Wishing you an awesome day & an exciting year ahead.Thank you for all you do for me, Dad & our family..I love ❤️ you to the moon 🌙 & back.. & more 🌌 ","Love you heaps!","Your Mum ♥️♥️♥️♥️"],
     ];
 
-    	var random = Math.floor(Math.random() * words.length);
+      var random = Math.floor(Math.random() * words.length);
 
    document.getElementById("dear").innerHTML=words[random][0];
    document.getElementById("message").innerHTML=words[random][1];
